cf6453324751038b4ea325535e9c0d684a969831
VYOM WADHWANI <vyomwadhwani11@gmail.com>
2025-09-20 00:11:35 +0530

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,1 +1,41 @@
-import { Toaster } from "@/components/ui/toaster"; import { Toaster as Sonner } from "@/components/ui/sonner"; import { TooltipProvider } from "@/components/ui/tooltip"; import { QueryClient, QueryClientProvider } from "@tanstack/react-query"; import { BrowserRouter, Routes, Route } from "react-router-dom"; import Index from "./pages/Index"; import Directory from "./pages/Directory"; import Events from "./pages/Events"; import Donate from "./pages/Donate"; import SignIn from "./pages/SignIn"; import Register from "./pages/Register"; import AdminPortal from "./pages/AdminPortal"; import RecruiterPortal from "./pages/RecruiterPortal"; import NotFound from "./pages/NotFound"; const queryClient = new QueryClient(); const App = () => ( <QueryClientProvider client={queryClient}> <TooltipProvider> <Toaster /> <Sonner /> <BrowserRouter> <Routes> <Route path="/" element={<Index />} /> <Route path="/directory" element={<Directory />} /> <Route path="/events" element={<Events />} /> <Route path="/donate" element={<Donate />} /> <Route path="/signin" element={<SignIn />} /> <Route path="/register" element={<Register />} /> <Route path="/admin" element={<AdminPortal />} /> <Route path="/recruiter" element={<RecruiterPortal />} /> {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */} <Route path="*" element={<NotFound />} /> </Routes> </BrowserRouter> </TooltipProvider> </QueryClientProvider> ); export default App;import { Toaster } from "@/components/ui/toaster"; import { Toaster as Sonner } from "@/components/ui/sonner"; import { TooltipProvider } from "@/components/ui/tooltip"; import { QueryClient, QueryClientProvider } from "@tanstack/react-query"; import { BrowserRouter, Routes, Route } from "react-router-dom"; import Index from "./pages/Index"; import Directory from "./pages/Directory"; import Events from "./pages/Events"; import Donate from "./pages/Donate"; import SignIn from "./pages/SignIn"; import Register from "./pages/Register"; import AdminPortal from "./pages/AdminPortal"; import RecruiterPortal from "./pages/RecruiterPortal"; import NotFound from "./pages/NotFound"; const queryClient = new QueryClient(); const App = () => ( <QueryClientProvider client={queryClient}> <TooltipProvider> <Toaster /> <Sonner /> <BrowserRouter> <Routes> <Route path="/" element={<Index />} /> <Route path="/directory" element={<Directory />} /> <Route path="/events" element={<Events />} /> <Route path="/donate" element={<Donate />} /> <Route path="/signin" element={<SignIn />} /> <Route path="/register" element={<Register />} /> <Route path="/admin" element={<AdminPortal />} /> <Route path="/recruiter" element={<RecruiterPortal />} /> {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */} <Route path="*" element={<NotFound />} /> </Routes> </BrowserRouter> </TooltipProvider> </QueryClientProvider> ); export default App;
+import { Toaster as SonnerToaster } from "@/components/ui/sonner";
+import { TooltipProvider } from "@/components/ui/tooltip";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
+import Index from "./pages/Index";
+import Directory from "./pages/Directory";
+import Events from "./pages/Events";
+import Donate from "./pages/Donate";
+import SignIn from "./pages/SignIn";
+import Register from "./pages/Register";
+import AdminPortal from "./pages/AdminPortal";
+import RecruiterPortal from "./pages/RecruiterPortal";
+import NotFound from "./pages/NotFound";
+
+const queryClient = new QueryClient();
+
+const App = () => (
+  <QueryClientProvider client={queryClient}>
+    <TooltipProvider>
+      {/* ✅ Only one toaster */}
+      <SonnerToaster />
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Index />} />
+          <Route path="/directory" element={<Directory />} />
+          <Route path="/events" element={<Events />} />
+          <Route path="/donate" element={<Donate />} />
+          <Route path="/signin" element={<SignIn />} />
+          <Route path="/register" element={<Register />} />
+          <Route path="/admin" element={<AdminPortal />} />
+          <Route path="/recruiter" element={<RecruiterPortal />} />
+          {/* Catch-all route */}
+          <Route path="*" element={<NotFound />} />
+        </Routes>
+      </BrowserRouter>
+    </TooltipProvider>
+  </QueryClientProvider>
+);
+
+export default App;
